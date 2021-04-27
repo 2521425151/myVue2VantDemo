ad4e56f2df11aa86904732fe06458a092ecb2593
hebaogan <2521425151@qq.com>
2021-04-27 15:48:01 +0800

    feat: router 更改

--- src/router/modules/app.ts
+++ src/router/modules/app.ts
@@ -1,18 +1,11 @@
 const OrderRouter = [
   {
-    path: '/cost',
-    component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue'),
-    redirect: '/cost/orderList',
-    children: [
-      {
-        path: 'orderList',
-        component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
-      },
-      {
-        path: 'orderDetail',
-        component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
-      }
-    ]
+    path: 'orderList',
+    component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
+  },
+  {
+    path: 'orderDetail',
+    component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
   }
 ]
 export default OrderRouter
