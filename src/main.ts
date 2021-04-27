5805f12694b14bab63254b19caea54b5575df8e4
hebaogan <2521425151@qq.com>
2021-04-27 20:28:22 +0800

    feat: add mock

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 开发环境下使用 mock
+process.env.NODE_ENV !== 'production' && require('./mock/mock.ts')
 
 Vue.config.productionTip = false
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,14 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+<script>
+import { sendGet } from '@/utils/static/requestWay'
+export default {
+  created() {
+    // mock 接口测试
+    sendGet('/api/test').then(res => {
+      console.log(res)
+    })
+  }
+}
+</script>
